f66e2a8c2f0f8a3584696f0b3afa868993c299e9
Compila <compila@localhost.localdomain>
2021-01-19 10:46:28 -0300

    colocando driver

--- Robo.py
+++ Robo.py
@@ -4,3 +4,9 @@ from selenium.webdriver.firefox.options import Options
 
 
 print("Configurando navegador")
+option = Options()
+option.headless = True
+fp = webdriver.FirefoxProfile()
+driver = webdriver.Firefox(firefox_profile=fp, options=option)
+print("Executando...")
+driver.close()
